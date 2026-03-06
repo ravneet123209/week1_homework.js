93297dff2695f4132618a97859d390b030e8fd37
ravneet123209 <harpreetrandhawa70777@gmail.com>
2026-03-06 15:18:09 +0100

    Task 1 — Personal Introduction

---
+++ week1_homework.js
@@ -0,0 +1,10 @@
+//Task 1 — Personal Introduction
+
+//name,age,city variable are declared using 'let'. So, It can be changed if needed.
+let name = "Ravneet Kaur";  
+let age = 21;  
+let city = "Montreal"; 
+
+//Display a personal introduction message in the console
+
+console.log("Hi, my name is " + name + ", I am " + age + " years old and I live in " + city + ".");
